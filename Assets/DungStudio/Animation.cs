47b6f4788592bfe00d5686e6572f8f570346eabe
Lariod <nguyenhuythinh7@gmail.com>
2021-11-12 01:35:16 +0700

    Animation
    Layer

--- Assets/DungStudio/Animation.cs
+++ Assets/DungStudio/Animation.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Animation : MonoBehaviour
 {
+    [SerializeField] private int a;
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("hello world");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        a = 5;
+        var b = a;
     }
 }
