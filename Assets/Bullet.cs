397f0fd53f5f6291a23bf4fd56be584c53184f17
Lariod <nguyenhuythinh7@gmail.com>
2021-11-14 03:08:37 +0700

    completed explosion when collider and add potion

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -1,12 +1,15 @@
 using System;
+using System.Security.Cryptography;
 using Player;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     public int throwForce;
-
+    
     // [SerializeField] private GameObject throwPoint;
+    [SerializeField] private GameObject explosionFX;
     private Vector3 aimDir;
     private Rigidbody rbBall;
     private GameObject throwPoint;
@@ -26,10 +29,17 @@ public class Bullet : MonoBehaviour
         rbBall.useGravity = true;
         transform.rotation = throwPoint.transform.rotation;
         rbBall.AddForce(AimController.aimDir * throwForce, ForceMode.Impulse);
+        Invoke("RemoveDelay",5);
+    }
+
+    private void RemoveDelay()
+    {
+        Destroy(gameObject);
     }
 
     private void OnCollisionEnter(Collision other)
     {
+        Instantiate(explosionFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
