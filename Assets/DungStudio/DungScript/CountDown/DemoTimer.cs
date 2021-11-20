0ec9612905f187e3e6891dabafef77e6e65ea6c4
Lariod <nguyenhuythinh7@gmail.com>
2021-11-20 12:36:15 +0700

    Create Scene
    Stat 
    Setting Menu
    Virus

--- Assets/DungStudio/DungScript/CountDown/DemoTimer.cs
+++ Assets/DungStudio/DungScript/CountDown/DemoTimer.cs
@@ -6,22 +6,28 @@ public class DemoTimer : MonoBehaviour
 {
     [SerializeField] private Timer ClockTimer;
     [SerializeField] private int NumberTime;
-
-    // Start is called before the first frame update
+    
     void Start()
     {
-        ClockTimer
-            .SetDuration(NumberTime)
-            .OnBegin(() =>Debug.Log("Timer started"))
-            .OnChange((remainingSeconds) => Debug.Log("Timer changed : " + remainingSeconds))
-            .OnEnd(() => Debug.Log("Timer ended"))
-            .OnPause(IsPaused => Debug.Log("----Paused : "+ IsPaused))
-            .Begin();
+        StartCountingClock();
     }
-
     private void Update()
     {
-        if(Input.GetKeyDown("p"))
+        //PauseGame();
+    }
+    private void StartCountingClock()
+    {
+        ClockTimer
+                    .SetDuration(NumberTime)
+                    .OnBegin(() =>Debug.Log("Timer started"))
+                    .OnChange((remainingSeconds) => Debug.Log("Timer changed : " + remainingSeconds))
+                    .OnEnd(() => Debug.Log("Timer ended"))
+                    //.OnPause(IsPaused => Debug.Log("----Paused : "+ IsPaused))
+                    .Begin();
+    }
+    private void PauseGame()
+    {
+        if (Input.GetKeyDown("p"))
         {
             ClockTimer.SetPause(!ClockTimer.IsPause);
         }
